c163bd07708f4a7fddfdc73588b3a3ae5c0747fb
zidannaxkeren <zidannaxkeren@yahoo.com>
2024-08-11 16:25:01 +0300

    Update e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :JSProj="JSProj" />
+</template>
+
+<script>
+import Vue from 'vue'
+import JSProj from '@JSProj/core'
+import { Dashboard } from '@JSProj/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    JSProj: () => new JSProj(),
+  },
+}
+</script>
+
+<style src="@JSProj/core/dist/style.css"></style>
+<style src="@JSProj/dashboard/dist/style.css"></style>
